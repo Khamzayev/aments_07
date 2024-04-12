4b417e951645e6ec8a80165ec2a12be1fa0cacfc
Khamzayev <xamzayevnodirbek@gmail.com>
2024-04-12 15:38:24 +0500

    models.py  add post model

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,19 @@ class Author(BaseModel):
     class Meta:
         verbose_name = _("Author")
         verbose_name_plural = _("1.Authors")
+
+        
+class Post(BaseModel):
+    title = models.CharField(max_length=100, help_text=_("write title"), verbose_name=_("title"))
+    author = models.CharField(max_length=30, help_text=_("write author name"), verbose_name=_("author"))
+    description = models.TextField(verbose_name=_("description"))
+    article = models.CharField(max_length=255, verbose_name=_("article"))
+    tag = models.ManyToManyField("Tags", verbose_name=_("tag"), related_name="posts")
+    image = models.ImageField(upload_to='media/', verbose_name=_("image"))
+
+    def __str__(self) -> str:
+        return f"{self.title[:15]}"
+    
+    class Meta:
+        verbose_name = _("Post")
+        verbose_name_plural = _("2.Posts")
